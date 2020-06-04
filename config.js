74575273e17431d93167b4d689c93dc809f12019
hezedu <hezedu@hotmail.com>
2020-06-04 21:20:42 +0800

    cert.pem -> fullchain.pem

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   port : 3001,
   secure: {
-    keyPath: '/opt/linux-remote/ssl/demo.linux-remote.org/privkey.pem',
-    certPath: '/etc/letsencrypt/live/demo.linux-remote.org/cert.pem'
+    keyPath: '/etc/letsencrypt/live/demo.linux-remote.org/privkey.pem',
+    certPath: '/etc/letsencrypt/live/demo.linux-remote.org/fullchain.pem'
   },
   MAX_USERS: 30
 }
