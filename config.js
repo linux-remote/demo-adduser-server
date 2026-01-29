23effcb753d105697226f89aabf729bc67ea6bc5
hezedu <hezedu@hotmail.com>
2026-01-29 22:50:40 +0800

    u

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 module.exports = {
   port : 3001,
-  secure: {
-    keyPath: '/etc/letsencrypt/live/demo.linux-remote.org/privkey.pem',
-    certPath: '/etc/letsencrypt/live/demo.linux-remote.org/fullchain.pem'
-  },
+  secure: null,
   MAX_USERS: 30,
   BEFORE_LOGIN_KEEP_TIME: 1000 * 60 * 10
 }
